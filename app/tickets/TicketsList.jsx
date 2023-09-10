ca086f353d3d1b5fb3749f5271ea23f228663a3d
Sitch196 <sichinava1996@gmail.com>
2023-09-10 21:42:49 +0400

    another one

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const TicketsList = () => {
   const [tickets, setTickets] = useState([]);
@@ -25,13 +26,18 @@ const TicketsList = () => {
     <div>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
+      {tickets.length === 0 && (
+        <p className="text-center"> There are no Tickets</p>
+      )}
     </div>
   );
 };
---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function TicketDetails({ params }) {
+  const [pageFound, setPageFound] = useState(true);
+  const [ticket, setTicket] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`http://localhost:4000/tickets/${params.id}`);
+        if (!res.ok) {
+          setPageFound(false);
+          return;
+        }
+
+        const tickets = await res.json();
+        setTicket(tickets);
+      } catch (error) {
+        console.error("Error fetching ticket:", error);
+        setPageFound(false);
+      }
+    };
+
+    fetchData();
+  }, [params.id]);
+
+  return (
+    <>
+      {pageFound ? (
+        <main>
+          <nav>
+            <h2>Ticket Details</h2>
+          </nav>
+          {ticket ? (
+            <div className="card">
+              <h3>{ticket.title}</h3>
+              <small>Created By {ticket.user_email}</small>
+              <p>{ticket.body}</p>
+              <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} priority
+              </div>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </main>
+      ) : (
+        <p>Page Not Found</p>
+      )}
+    </>
+  );
+}
